dcf6f1fd5ada1a52a09cf3c7e05d59fc7f8d70d7
matteowong <mwong8@stuy.edu>
2017-10-02 21:15:57 -0400

    n versions cpy/cat

--- strstuff.c
+++ strstuff.c
@@ -12,25 +12,41 @@ int mystrlen(char * s) {
 
 char * mystrcpy( char *dest, char *source ) {
   int src_count=0;
-  while (source[src_count])
-    dest[src_count]=source[src_count++];
+  while (source[src_count]) {
+    dest[src_count]=source[src_count];
+    src_count++;
+  }
   dest[src_count]=0;
   return dest;
 }
 
 char * mystrncpy( char *dest, char *source, int n ) {
   int src_count=0;
-  while (src_count<n)
-    dest[src_count]=source[src_count++];
+  while (src_count<n) {
+    dest[src_count]=source[src_count];
+    src_count++;
+  }
   return dest;
 }
 
 char * mystrcat(char * dest,char * source) {
   int src_count=0;
   int dest_len=mystrlen(dest);
-  while (source[src_count])
-    dest[dest_len+src_count]=source[src_count++];
+  while (source[src_count]) {
+    dest[dest_len+src_count]=source[src_count];
+    src_count++;
+  }
   dest[dest_len+src_count]=0;
+  return dest;
+}
+
+char * mystrncat(char * dest,char * source, int n) {
+  int src_count=0;
+  int dest_len=mystrlen(dest);
+  while (src_count<n) {
+    dest[dest_len+src_count]=source[src_count];
+    src_count++;
+  }
   return dest;
 }
 
@@ -58,9 +74,19 @@ int main() {
   printf("mystrcpy: [%s]\n\n",mystrcpy(s1,s2));
 
   strreset(s1);
+  printf("Testing strncpy(s1,\"archipelago\",4)\n");
+  printf("strncpy: [%s]\n",strncpy(s1,"archipelago",4));
+  strreset(s1);
+  printf("mystrncpy: [%s]\n\n",mystrncpy(s1,"archipelago",4));
+
+  strreset(s1);
   printf("Testing strcat(s1,s2)\n");
   printf("strcat: [%s]\n",strcat(s1,s2));
-  printf("mystrcat: [%s]\n",mystrcat(s1,s2));
+  printf("mystrcat: [%s]\n\n",mystrcat(s1,s2));
+
+  printf("Testing strncat(s1,s2,3)\n");
+  printf("strncat: [%s]\n",strncat(s1,s2,3));
+  printf("mystrncat: [%s]\n\n",mystrncat(s1,s2,3));
 
   return 0;
 }
