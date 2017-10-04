6b2489a225bc9d729b9042de84f9522eb50b39ad
roger778876 <rogercesar778876@gmail.com>
2017-10-03 23:33:44 -0400

    add mystrchr

--- cesar.c
+++ cesar.c
@@ -3,36 +3,67 @@
 
 
 int mystrlen(char *a){
-    int output = 0;
-    while (*(a++)){
-        output++;
-    }
-    return output;
+  int output = 0;
+  while (*(a++)){
+    output++;
+  }
+  return output;
 }
 
+// strcmp
 int mystrcmp(char *s1, char *s2){
-    int i = 0;
-    while (s1[i] != 0 && s2[i] != 0){
-        int diff = s1[i] - s2[i];
-        if (diff == 0){
-            i++;
-        }
-        else {
-            return diff;
-        }
-    }
-    if (mystrlen(s1) == mystrlen(s2)){
-        return 0;
+  int i = 0;
+  while (s1[i] != 0 && s2[i] != 0){
+    int diff = s1[i] - s2[i];
+    if (diff == 0){
+      i++;
     }
     else {
-        return (mystrlen(s1) - mystrlen(s2));
+      return diff;
     }
+  }
+  if (mystrlen(s1) == mystrlen(s2)){
+    return 0;
+  }
+  else {
+    return (mystrlen(s1) - mystrlen(s2));
+  }
+}
+
+// strchr
+char * mystrchr(char *s, char c){
+  char *output = s;
+  while (*output){
+    if (*output == c){
+      return output;
+    }
+    output++;
+  }
+  return 0;
 }
 
 int main(){
-    char *s1 = "ab";
-    char *s2 = "abc";
-    int real = strcmp(s1, s2);
-    int my = mystrcmp(s1, s2);
-    printf("real: %d\nmine: %d", real, my); 
+  char *s1 = "ab";
+  char *s2 = "abc";
+  char c = 'c';
+
+  // testing mystrcmp
+  printf("Testing mystrcmp(char *s1, char *s2)\n");
+  printf("s1 = [%s]  s2 = [%s]\n", s1, s2);
+  printf("default value: %d\n", strcmp(s1, s2));
+  printf("my value: %d\n", mystrcmp(s1, s2));
+  
+  printf("\n");
+  // testing mystrchr
+  printf("Testing mystrchr(char *s, char c)\n");
+  printf("s1 = [%s]  c = [%c]\n", s1, c);
+  printf("default value: %p\n", strchr(s1, c));
+  printf("my value: %p\n", mystrchr(s1, c));
+
+  printf("\n");
+  
+  printf("Testing mystrchr(char *s, char c)\n");
+  printf("s1 = [%s]  c = [%c]\n", s2, c);
+  printf("default value: %p\n", strchr(s2, c));
+  printf("my value: %p\n", mystrchr(s2, c));
 }
